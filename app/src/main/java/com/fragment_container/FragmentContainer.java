06dbab2c5ccdafd2ebe782223115c6271591852d
PPFeiZhou <pengfei_zhou555@163.com>
2018-04-23 23:22:25 +0800

    add recyclerView to every navigationBar Item add FragmentAdapter.java add fragmentContainer.java add ViewpagerAdapter.java add anim(fade_in.xml/fade_out.xml) add fragment_hotel.xml add fragment_repast.xml add fragment_sight.xml add fragment_travel.xml add fragment_user.xml add layout_item.xml

---
+++ app/src/main/java/com/fragment_container/FragmentContainer.java
@@ -0,0 +1,171 @@
+package com.fragment_container;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+
+import com.qinzitravel.R;
+
+import java.util.ArrayList;
+
+public class FragmentContainer extends Fragment {
+
+    private FrameLayout fragmentContainer;
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+
+    public static FragmentContainer newInstance(int index) {
+        FragmentContainer fragmentContainer = new FragmentContainer();
+        Bundle b = new Bundle();
+        b.putInt("index", index);
+        fragmentContainer.setArguments(b);
+        return fragmentContainer;
+    }
+
+    @Nullable
+    @Override
+    /**
+     * load selected navigation item content
+     */
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if (getArguments().getInt("index", 0) == 0) {
+            View view = inflater.inflate(R.layout.fragment_sight, container, false);
+            initSight(view);
+            return view;
+        } else if (getArguments().getInt("index", 1) == 1) {
+            View view = inflater.inflate(R.layout.fragment_travel, container, false);
+            initTravel(view);
+            return view;
+        } else if (getArguments().getInt("index", 2) == 2) {
+            View view = inflater.inflate(R.layout.fragment_repast, container, false);
+            initRepast(view);
+            return view;
+        } else if (getArguments().getInt("index", 3) == 3) {
+            View view = inflater.inflate(R.layout.fragment_hotel, container, false);
+            initHotel(view);
+            return view;
+        } else {
+            View view = inflater.inflate(R.layout.fragment_user, container, false);
+            initUser(view);
+            return view;
+        }
+    }
+
+    private void initUser(View view) {
+        fragmentContainer = view.findViewById(R.id.fragment_user);
+        recyclerView = view.findViewById(R.id.fragment_user_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        ArrayList<String> itemsData = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            itemsData.add("Fragment " + getArguments().getInt("index", -1) + " / Item " + i);
+        }
+
+        FragmentAdapter adapter = new FragmentAdapter(itemsData);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void initHotel(View view) {
+        fragmentContainer = view.findViewById(R.id.fragment_hotel);
+        recyclerView = view.findViewById(R.id.fragment_hotel_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        ArrayList<String> itemsData = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            itemsData.add("Fragment " + getArguments().getInt("index", -1) + " / Item " + i);
+        }
+
+        FragmentAdapter adapter = new FragmentAdapter(itemsData);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void initRepast(View view) {
+        fragmentContainer = view.findViewById(R.id.fragment_repast);
+        recyclerView = view.findViewById(R.id.fragment_repast_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        ArrayList<String> itemsData = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            itemsData.add("Fragment " + getArguments().getInt("index", -1) + " / Item " + i);
+        }
+
+        FragmentAdapter adapter = new FragmentAdapter(itemsData);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void initTravel(View view) {
+        fragmentContainer = view.findViewById(R.id.fragment_travel);
+        recyclerView = view.findViewById(R.id.fragment_travel_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        ArrayList<String> itemsData = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            itemsData.add("Fragment " + getArguments().getInt("index", -1) + " / Item " + i);
+        }
+
+        FragmentAdapter adapter = new FragmentAdapter(itemsData);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void initSight(View view) {
+        fragmentContainer = view.findViewById(R.id.fragment_sight);
+        recyclerView = view.findViewById(R.id.fragment_sight_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        ArrayList<String> itemsData = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            itemsData.add("Fragment " + getArguments().getInt("index", -1) + " / Item " + i);
+        }
+
+        FragmentAdapter adapter = new FragmentAdapter(itemsData);
+        recyclerView.setAdapter(adapter);
+    }
+
+    /**
+     * Refresh
+     */
+    public void refresh() {
+        if (getArguments().getInt("index", 0) > 0 && recyclerView != null) {
+            recyclerView.smoothScrollToPosition(0);
+        }
+    }
+
+    /**
+     * Called when a fragment will be displayed
+     */
+    public void willBeDisplayed() {
+        // Do what you want here, for example animate the content
+        if (fragmentContainer != null) {
+            Animation fadeIn = AnimationUtils.loadAnimation(getActivity(), R.anim.fade_in);
+            fragmentContainer.startAnimation(fadeIn);
+        }
+    }
+
+    /**
+     * Called when a fragment will be hidden
+     */
+    public void willBeHidden() {
+        if (fragmentContainer != null) {
+            Animation fadeOut = AnimationUtils.loadAnimation(getActivity(), R.anim.fade_out);
+            fragmentContainer.startAnimation(fadeOut);
+        }
+    }
+}
---
+++ app/src/main/java/com/viewPagerAdapter/ViewPagerAdapter.java
@@ -0,0 +1,53 @@
+package com.viewPagerAdapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.view.ViewGroup;
+
+import com.fragment_container.FragmentContainer;
+
+import java.util.ArrayList;
+
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private ArrayList<FragmentContainer> fragments = new ArrayList<>();
+    private FragmentContainer currentFragment;
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+
+        fragments.clear();
+        fragments.add(FragmentContainer.newInstance(0));
+        fragments.add(FragmentContainer.newInstance(1));
+        fragments.add(FragmentContainer.newInstance(2));
+        fragments.add(FragmentContainer.newInstance(3));
+        fragments.add(FragmentContainer.newInstance(4));
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    @Override
+    public void setPrimaryItem(ViewGroup container, int position, Object object) {
+        if (getCurrentFragment() != object) {
+            currentFragment = ((FragmentContainer) object);
+        }
+        super.setPrimaryItem(container, position, object);
+    }
+
+    /**
+     * Get the current fragment
+     */
+    public FragmentContainer getCurrentFragment() {
+        return currentFragment;
+    }
+}
